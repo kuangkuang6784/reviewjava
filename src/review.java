fc40e0fc8e535c33a2110072b16c3b4baf768e0a
kuangkuang <642806784@qq.com>
2019-05-06 19:33:54 +0800

    20190506第一次提交
    Signed-off-by: KuangKuang <642806784@qq.com>

--- src/review.java
+++ src/review.java
@@ -1,6 +1,7 @@
 import java.lang.*;
 import java.util.*;
 import java.text.*;
+import java.io.*;
 
 public class review{
     //数组作为函数参数
@@ -37,7 +38,8 @@ public class review{
         System.out.println("The max value is " + result);
     }
 
-    public static void main(String args[]){
+    //public static void main(String args[]){
+    public static void main(String args[])throws IOException{
         //System.out.println("test1");
 
         //Number类型
@@ -74,7 +76,87 @@ public class review{
         */
 
         //可变参数
+        /*
         printMax(34, 3, 3, 2, 56.5);
         printMax(new double[]{1, 2, 3});
+        */
+
+        /*
+        //使用 BufferedReader 在控制台读取字符
+        char c;
+        // 使用 System.in 创建 BufferedReader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("输入字符, 按下 'q' 键退出。");
+        // 读取字符
+        do {
+            c = (char) br.read();
+            System.out.println(c);
+        } while (c != 'q');
+        */
+
+        //文件读写
+        //File f = new File("../resource/test.txt");
+        //OutputStream f = new FileOutputStream(f);
+        /*
+        //以二进制形式读写，产生乱码
+        try {
+            byte bWrite[] = { 11, 21, 3, 40, 5 };
+            OutputStream os = new FileOutputStream("resource/test.txt");
+            for (int x = 0; x < bWrite.length; x++) {
+                os.write(bWrite[x]); // writes the bytes
+            }
+            os.close();
+
+            InputStream is = new FileInputStream("resource/test.txt");
+            int size = is.available();
+
+            for (int i = 0; i < size; i++) {
+                System.out.print((char) is.read() + "  ");
+            }
+            is.close();
+        } catch (IOException e) {
+            System.out.print("Exception");
+        }
+        */
+        File f = new File("resource/test2.txt");
+        FileOutputStream fop = new FileOutputStream(f);
+        // 构建FileOutputStream对象,文件不存在会自动新建
+
+        OutputStreamWriter writer = new OutputStreamWriter(fop, "UTF-8");
+        // 构建OutputStreamWriter对象,参数可以指定编码,默认为操作系统默认编码,windows上是gbk
+
+        writer.append("中文输入");
+        // 写入到缓冲区
+
+        writer.append("\r\n");
+        // 换行
+
+        writer.append("English");
+        // 刷新缓存冲,写入到文件,如果下面已经没有写入的内容了,直接close也会写入
+
+        writer.close();
+        // 关闭写入流,同时会把缓冲区内容写入文件,所以上面的注释掉
+
+        fop.close();
+        // 关闭输出流,释放系统资源
+
+        FileInputStream fip = new FileInputStream(f);
+        // 构建FileInputStream对象
+
+        InputStreamReader reader = new InputStreamReader(fip, "UTF-8");
+        // 构建InputStreamReader对象,编码与写入相同
+
+        StringBuffer sb = new StringBuffer();
+        while (reader.ready()) {
+            sb.append((char) reader.read());
+            // 转成char加到StringBuffer对象中
+        }
+        System.out.println(sb.toString());
+        reader.close();
+        // 关闭读取流
+
+        fip.close();
+        // 关闭输入流,释放系统资源
+
     }
 }
